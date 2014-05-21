b18c353f66f91853ba45671f5810050fe7f6e80b
Lay Kuan Loh <lkloh2410@gmail.com>
2014-05-20 22:17:10 -0500

    saving

--- fourier.py
+++ fourier.py
@@ -1,7 +1,26 @@
-from matplotlib import *
+
 from numpy import *
+import matplotlib.pyplot as py
+from numpy.fft import *
+
+ot = arange(-20,20,0.25)
+ots = 4*sin(ot/2) + 2.4*cos(ot*8) + 5*cos(ot*2)
 
 
-original_time = arange(-20,20,0.25)
+# plot origingal signal-vs-time
+fig = py.figure()
+fig.suptitle('FILTERING FREQUENCIES')
 
-print original_time
+ax1 = fig.add_subplot(211)
+ax1.plot(ot,ots)
+ax1.set_title('Amplitude vs Time')
+
+ofs = fft(ots)
+of = fftfreq(len(ots), 0.25)
+
+ax2 = fig.add_subplot(212)
+ax2.plot(of,ofs)
+ax2.set_title('Amplitude Spectrum vs frequency')
+
+py.show()
+
