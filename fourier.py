788780e252bfabc782b5c0b6e45386ca0d266fdb
Lay Kuan Loh <lkloh2410@gmail.com>
2014-05-20 23:26:29 -0500

    better names

--- fourier.py
+++ fourier.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as py
 from numpy.fft import *
 from scipy import signal
 
-ot = arange(-20,20,0.25)
-ots = 4*sin(ot/2) + 2.4*cos(ot*8) + 5*cos(ot*2)
+originalTime = arange(-20,20,0.25)
+originalSignalTime= 4*sin(originalTime/2) + 2.4*cos(originalTime*8) + 5*cos(originalTime*2)
 
 
 # plot origingal signal-vs-time
@@ -14,34 +14,34 @@ fig.suptitle('FILTERING FREQUENCIES')
 
 # time -> frequency 
 # UNFILTERED
-ofs = fft(ots)
-of = fftfreq(len(ots), 0.25)
+originalSignalFreq = fft(originalSignalTime)
+originalFreq = fftfreq(len(originalSignalTime), 0.25)
 
-am_freq = [nan]*len(ots)
-for i in xrange(len(ofs)):
-	am_freq[i] = ofs[i].real**2 + ofs[i].imag**2
+amplitudeFreq = [nan]*len(originalSignalTime)
+for i in xrange(len(originalSignalFreq)):
+	amplitudeFreq [i] = originalSignalFreq[i].real**2 + originalSignalFreq[i].imag**2
 
 #filter
 B, A = signal.butter(2,[0.03,0.2],btype='bandpass')
 
 w, h = signal.freqz(B,A)
-filteredSignalFrequency = signal.lfilter(B,A,am_freq)
+filteredSignalFrequency = signal.lfilter(B,A,amplitudeFreq)
 
-# frequenc -> time
+# frequency -> time
 # filtered
 filteredSignalTime = ifft(filteredSignalFrequency)
 
 ax1 = fig.add_subplot(211)
-ax1.plot(ot,ots,label='Original')
-ax1.plot(ot,filteredSignalTime,label='Filtered')
+ax1.plot(originalTime,originalSignalTime,label='Original')
+ax1.plot(originalTime,filteredSignalTime,label='Filtered')
 ax1.legend(loc="upper right")
 ax1.set_title('Amplitude vs Time')
 
 
 ax2 = fig.add_subplot(212)
-ax2.plot(of,am_freq,label='Original')
-ax2.plot(of,filteredSignalFrequency,label='Filtered')
-MULTIPLE = 0.7*max(am_freq)
+ax2.plot(originalFreq,amplitudeFreq ,label='Original')
+ax2.plot(originalFreq,filteredSignalFrequency,label='Filtered')
+MULTIPLE = 0.7*max(amplitudeFreq)
 ax2.plot(w,MULTIPLE*h,label='Butter Filter')
 ax2.legend(loc="upper right")
 ax2.set_title('Amplitude Spectrum vs frequency')
