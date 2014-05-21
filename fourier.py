424512df87b3f4a10a5edb012902c79dcc68715e
Lay Kuan Loh <lkloh2410@gmail.com>
2014-05-20 22:40:12 -0500

    did not remove

--- fourier.py
+++ fourier.py
@@ -2,6 +2,7 @@
 from numpy import *
 import matplotlib.pyplot as py
 from numpy.fft import *
+from scipy import signal
 
 ot = arange(-20,20,0.25)
 ots = 4*sin(ot/2) + 2.4*cos(ot*8) + 5*cos(ot*2)
@@ -18,8 +19,26 @@ ax1.set_title('Amplitude vs Time')
 ofs = fft(ots)
 of = fftfreq(len(ots), 0.25)
 
+#filter
+order = 2
+lowFreq = 1.0
+highFreq = 1.5
+B, A = signal.butter(2,[0.5,1.0],btype='bandpass')
+
+print A
+
+w, h = signal.freqz(B,A)
+# w, h = signal.freqs(B,A)
+ff = signal.lfilter(B,A,ofs)
+# 
+
+# print ff
+
 ax2 = fig.add_subplot(212)
-ax2.plot(of,ofs)
+ax2.plot(of,ofs,label='Original')
+ax2.plot(of,ff,label='Filtered')
+ax2.legend(loc="lower left")
+
 ax2.set_title('Amplitude Spectrum vs frequency')
 
 py.show()
