0946bcd4f740b0f3d9937ebe9a706cee1dd810d6
Lay Kuan Loh <lkloh2410@gmail.com>
2014-05-22 11:14:56 -0500

    fixed filtering

--- fourier.py
+++ fourier.py
@@ -1,11 +1,10 @@
 
-from numpy import *
+import numpy as np
 import matplotlib.pyplot as py
-from numpy.fft import *
 from scipy import signal
 
-originalTime = arange(-20,20,0.25)
-originalSignalTime= 4*sin(originalTime/2) + 2.4*cos(originalTime*8) + 5*cos(originalTime*2)
+originalTime = np.arange(-20,20,0.25)
+originalSignalTime= 4*np.sin(originalTime/2) + 2.4*np.cos(originalTime*8) + 5*np.cos(originalTime*2)
 
 
 # plot origingal signal-vs-time
@@ -14,22 +13,29 @@ fig.suptitle('FILTERING FREQUENCIES')
 
 # time -> frequency 
 # UNFILTERED
-originalSignalFreq = fft(originalSignalTime)
-originalFreq = fftfreq(len(originalSignalTime), 0.25)
+originalSignalFreq = np.fft.fft(originalSignalTime)
+originalFreq = np.fft.fftfreq(len(originalSignalTime), 0.25)
 
-amplitudeFreq = [nan]*len(originalSignalTime)
+#amplitudeFreq = [nan]*len(originalSignalTime)
+amplitudeFreq = np.zeros([len(originalSignalTime)])
 for i in xrange(len(originalSignalFreq)):
-	amplitudeFreq[i] = sqrt(originalSignalFreq[i].real**2 + originalSignalFreq[i].imag**2)
+	amplitudeFreq[i] = np.sqrt(originalSignalFreq[i].real**2 + originalSignalFreq[i].imag**2)
 
 #filter
 B, A = signal.butter(2,[0.03,0.2],btype='bandpass')
 
 w, h = signal.freqz(B,A)
-filteredSignalFrequency = signal.lfilter(B,A,amplitudeFreq)
+filteredSignalTime = signal.lfilter(B,A,originalSignalTime)
 
 # frequency -> time
 # filtered
-filteredSignalTime = ifft(filteredSignalFrequency)
+filteredSignalFrequency = np.fft.fft(filteredSignalTime)
+
+# convered filtered freq signal => amplitude freq signal
+amplitudeFilteredFreq = np.zeros([len(filteredSignalFrequency)])
+for i in xrange(len(filteredSignalFrequency)):
+	amplitudeFilteredFreq[i] = np.sqrt(filteredSignalFrequency[i].real**2 + filteredSignalFrequency[i].imag**2)
+
 
 ax1 = fig.add_subplot(211)
 ax1.plot(originalTime,originalSignalTime,label='Original')
@@ -39,10 +45,10 @@ ax1.set_title('Amplitude vs Time')
 
 
 ax2 = fig.add_subplot(212)
-ax2.plot(originalFreq,amplitudeFreq ,label='Original')
-ax2.plot(originalFreq,filteredSignalFrequency,label='Filtered')
+ax2.plot(originalFreq/(2*np.pi),amplitudeFreq ,label='Original')
+ax2.plot(originalFreq/(2*np.pi),amplitudeFilteredFreq,label='Filtered')
 MULTIPLE = 0.7*max(amplitudeFreq)
-ax2.plot(w,MULTIPLE*h,label='Butter Filter')
+ax2.plot(w/(2*np.pi),MULTIPLE*np.abs(h),label='Butter Filter')
 ax2.legend(loc="upper right")
 ax2.set_title('Amplitude Spectrum vs frequency')
 
