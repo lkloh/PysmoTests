6e3c5011e45a09398a11413f33fc330fc483db1b
Lay Kuan Loh <lkloh2410@gmail.com>
2014-05-22 11:17:38 -0500

    abs takes care of it

--- fourier.py
+++ fourier.py
@@ -16,11 +16,6 @@ fig.suptitle('FILTERING FREQUENCIES')
 originalSignalFreq = np.fft.fft(originalSignalTime)
 originalFreq = np.fft.fftfreq(len(originalSignalTime), 0.25)
 
-#amplitudeFreq = [nan]*len(originalSignalTime)
-amplitudeFreq = np.zeros([len(originalSignalTime)])
-for i in xrange(len(originalSignalFreq)):
-	amplitudeFreq[i] = np.sqrt(originalSignalFreq[i].real**2 + originalSignalFreq[i].imag**2)
-
 #filter
 B, A = signal.butter(2,[0.03,0.2],btype='bandpass')
 
@@ -31,22 +26,16 @@ filteredSignalTime = signal.lfilter(B,A,originalSignalTime)
 # filtered
 filteredSignalFrequency = np.fft.fft(filteredSignalTime)
 
-# convered filtered freq signal => amplitude freq signal
-amplitudeFilteredFreq = np.zeros([len(filteredSignalFrequency)])
-for i in xrange(len(filteredSignalFrequency)):
-	amplitudeFilteredFreq[i] = np.sqrt(filteredSignalFrequency[i].real**2 + filteredSignalFrequency[i].imag**2)
-
-
 ax1 = fig.add_subplot(211)
 ax1.plot(originalTime,originalSignalTime,label='Original')
 ax1.plot(originalTime,filteredSignalTime,label='Filtered')
 ax1.legend(loc="upper right")
 ax1.set_title('Amplitude vs Time')
 
-
+# in Hertz
 ax2 = fig.add_subplot(212)
-ax2.plot(originalFreq/(2*np.pi),amplitudeFreq ,label='Original')
-ax2.plot(originalFreq/(2*np.pi),amplitudeFilteredFreq,label='Filtered')
+ax2.plot(originalFreq/(2*np.pi),np.abs(originalSignalFreq),label='Original')
+ax2.plot(originalFreq/(2*np.pi),np.abs(filteredSignalFrequency),label='Filtered')
 MULTIPLE = 0.7*max(amplitudeFreq)
 ax2.plot(w/(2*np.pi),MULTIPLE*np.abs(h),label='Butter Filter')
 ax2.legend(loc="upper right")
