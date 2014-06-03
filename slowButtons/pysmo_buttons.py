bf51fa6e594289dfbc0740c15af5734b4667b444
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-03 12:31:47 -0500

    need to get entire canvas

--- slowButtons/pysmo_buttons.py
+++ slowButtons/pysmo_buttons.py
@@ -11,23 +11,39 @@ class SetUp:
 		self.fig = py.figure(figsize=(5,5))
 
 		self.axs = self.addAxes()
+
+		self.bntext = py.Button(self.axs['text'], 'Text')
+		self.cidtext = self.bntext.on_clicked(self.texting)
+
 		self.bnquit = py.Button(self.axs['change'], 'Change')
 		self.cidquit = self.bnquit.on_clicked(self.changed)
+
 		py.show()
+
+	def texting(self, event):
+		print self.bntext.label.get_text()
+		if self.bntext.label.get_text() == 'Text':
+			self.bntext.label.set_text('LOL')
+		else:
+			self.bntext.label.set_text('Text')
 
 	def addAxes(self):
 		axs = {}
 		rectchange = [0.20, 0.50, 0.20, 0.05]
 		axs['change'] = self.fig.add_axes(rectchange)
+		recttext = [0.20, 0.30, 0.20, 0.05]
+		axs['text'] = self.fig.add_axes(recttext)
 		return axs
 
 	def changed(self,event):
 		print self.bnquit.label.get_text()
 		if self.bnquit.label.get_text() == 'Change':
 			self.bnquit.label.set_text('Hello')
+			self.bntext.label.set_text('LOL')
 		else:
 			self.bnquit.label.set_text('Change')
-		self.fig.canvas.draw()
+		#self.fig.canvas.draw()
+		self.axs['change'].figure.canvas.draw()
 
 def main():
 	setup = SetUp()
