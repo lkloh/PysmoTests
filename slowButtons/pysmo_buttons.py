a92bf6f1c48252794120e0f787fdb7c507cdc6d6
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-03 10:28:47 -0500

    just like befor

---
+++ slowButtons/pysmo_buttons.py
@@ -0,0 +1,33 @@
+import matplotlib, sys
+matplotlib.use('TkAgg')
+from numpy import arange, sin, pi
+import matplotlib.pyplot as py
+
+class SetUp:
+
+	def __init__(self):
+		self.fig = py.figure(figsize=(5,5))
+
+		self.axs = self.addAxes()
+		self.bnquit = py.Button(self.axs['change'], 'Change')
+		self.cidquit = self.bnquit.on_clicked(self.changed)
+		py.show()
+
+	def addAxes(self):
+		axs = {}
+		rectchange = [0.20, 0.50, 0.20, 0.05]
+		axs['change'] = self.fig.add_axes(rectchange)
+		return axs
+
+	def changed(self,event):
+		if self.bnquit.label.text == 'Change':
+			self.bnquit.label.set_text('Hello')
+		else:
+			self.bnquit.label.set_text('Change')
+
+def main():
+	setup = SetUp()
+
+
+if __name__ == "__main__":
+	main()
