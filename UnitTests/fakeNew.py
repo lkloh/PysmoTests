089675a3d9c1e76d596802a54e2c1200001665ef
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-09 15:30:00 -0500

    ned

--- UnitTests/fakeNew.py
+++ UnitTests/fakeNew.py
@@ -4,10 +4,15 @@ import datetime
 
 class FooTests(unittest.TestCase):
 
-	def testMatches(self):
+	def testMatchesFalse(self):
 		p = DatePattern(2014, 9, 28)
 		d = datetime.date(2014, 9, 29)
 		self.failIf(p.matches(d))
+
+	def testMatchesTrue(self):
+		p = DatePattern(2014, 9, 28)
+		d = datetime.date(2014, 9, 28)
+		self.failUnless(p.matches(d))
 
 def main():
 	unittest.main()
