d9ff8c862df934b0358cc3d80e2cd55eb60cf3e9
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-10 19:56:27 -0500

    test

---
+++ fakeMouseEvents/faker.py
@@ -0,0 +1,53 @@
+import matplotlib, sys
+matplotlib.use('TkAgg')
+# Qt4Agg
+from numpy import arange, sin, pi
+import matplotlib.pyplot as py
+# http://stackoverflow.com/questions/23167424/matplotlib-button-color-updates-only-after-moving-mouse
+
+class SetUp:
+
+	def __init__(self):
+		self.fig = py.figure(figsize=(5,5))
+
+		self.axs = self.addAxes()
+
+		self.bntext = py.Button(self.axs['text'], 'Text')
+		self.cidtext = self.bntext.on_clicked(self.texting)
+
+		self.bnchange= py.Button(self.axs['change'], 'Change')
+		self.cidchange = self.bnchange.on_clicked(self.changed)
+
+		py.show()
+
+	def texting(self, event):
+		print self.bntext.label.get_text()
+		if self.bntext.label.get_text() == 'Text':
+			self.bntext.label.set_text('LOL')
+		else:
+			self.bntext.label.set_text('Text')
+
+	def addAxes(self):
+		axs = {}
+		rectchange = [0.20, 0.50, 0.20, 0.05]
+		axs['change'] = self.fig.add_axes(rectchange)
+		recttext = [0.20, 0.30, 0.20, 0.05]
+		axs['text'] = self.fig.add_axes(recttext)
+		return axs
+
+	def changed(self,event):
+		print self.bnchange.label.get_text()
+		if self.bnchange.label.get_text() == 'Change':
+			self.bnchange.label.set_text('Hello')
+			self.bntext.label.set_text('LOL')
+		else:
+			self.bnchange.label.set_text('Change')
+		print type(self.axs['text'])
+		event.canvas.blit(self.axs['text'].bbox)
+
+def main():
+	setup = SetUp()
+
+
+if __name__ == "__main__":
+	main()
---
+++ fakeMouseEvents/test_faker.py
@@ -0,0 +1,13 @@
+import unittest
+from faker import SetUp
+
+class FakerTests(unittest.TestCase):
+
+	def testMatchesWeekday(self):
+		self.assertTrue(1==1)
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
