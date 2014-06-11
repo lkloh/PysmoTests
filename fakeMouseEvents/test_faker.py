77da062aca3e936938c5f3c1e93f42372e331368
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-10 20:41:46 -0500

    I COPIED IF

--- fakeMouseEvents/test_faker.py
+++ fakeMouseEvents/test_faker.py
@@ -1,9 +1,12 @@
 import unittest
-from faker import SetUp
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
 
 class FakerTests(unittest.TestCase):
 
 	def testMatchesWeekday(self):
+		m = PyMouse()
+		k = PyKeyboard()
 		self.assertTrue(1==1)
 
 def main():
