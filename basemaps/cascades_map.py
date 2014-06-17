85c60820d1a2d576eb00abdc489674753504c918
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-17 10:26:52 -0500

    no more dots

--- basemaps/cascades_map.py
+++ basemaps/cascades_map.py
@@ -1,23 +1,33 @@
-
+"""copied from http://matplotlib.org/basemap/users/lcc.html"""
 import matplotlib
 matplotlib.rcParams['backend'] = "TkAgg"
 
+from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-
-# Lambert Conformal Conic map.
-m = Basemap(llcrnrlon=-100.,llcrnrlat=0.,
-            urcrnrlon=-20.,urcrnrlat=57.,
-            projection='lcc',
-            lat_1=20.,lat_2=40.,lon_0=-60.,
-            resolution ='l',area_thresh=1000.)
-
+# setup lambert conformal basemap.
+# lat_1 is first standard parallel.
+# lat_2 is second standard parallel (defaults to lat_1).
+# lon_0,lat_0 is central point.
+# rsphere=(6378137.00,6356752.3142) specifies WGS4 ellipsoid
+# area_thresh=1000 means don't plot coastline features less
+# than 1000 km^2 in area.
+m = Basemap(width=12000000,height=9000000,
+            rsphere=(6378137.00,6356752.3142),\
+            resolution='l',area_thresh=1000.,projection='lcc',\
+            lat_1=45.,lat_2=55,lat_0=50,lon_0=-107.)
 m.drawcoastlines()
-m.drawcountries()
-m.drawmapboundary(fill_color='#99ffff')
-m.fillcontinents(color='#cc9966',lake_color='#99ffff')
-m.drawparallels(np.arange(10,70,20),labels=[1,1,0,0])
-m.drawmeridians(np.arange(-100,0,20),labels=[0,0,0,1])
-plt.title('Cascades')
+m.fillcontinents(color='coral',lake_color='aqua')
+# draw parallels and meridians.
+m.drawparallels(np.arange(-80.,81.,20.))
+m.drawmeridians(np.arange(-180.,181.,20.))
+m.drawmapboundary(fill_color='aqua')
+# draw tissot's indicatrix to show distortion.
+ax = plt.gca()
+# for y in np.linspace(m.ymax/20,19*m.ymax/20,9):
+#     for x in np.linspace(m.xmax/20,19*m.xmax/20,12):
+#         lon, lat = m(x,y,inverse=True)
+#         poly = m.tissot(lon,lat,1.5,100,\
+#                         facecolor='green',zorder=10,alpha=0.5)
+plt.title("Lambert Conformal Projection")
 plt.show()
