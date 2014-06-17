cf03105710872304b9085277c1b64f69b57aba4a
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-17 10:11:24 -0500

    cascades

--- basemaps/cascades_map.py
+++ basemaps/cascades_map.py
@@ -1,10 +1,4 @@
-"""
-draw Atlantic Hurricane Tracks for storms that reached Cat 4 or 5.
-part of the track for which storm is cat 4 or 5 is shown red.
-ESRI shapefile data from http://nationalatlas.gov/mld/huralll.html
 
-copied from http://matplotlib.org/basemap/users/examples.html
-"""
 import matplotlib
 matplotlib.rcParams['backend'] = "TkAgg"
 
@@ -25,5 +19,5 @@ m.drawmapboundary(fill_color='#99ffff')
 m.fillcontinents(color='#cc9966',lake_color='#99ffff')
 m.drawparallels(np.arange(10,70,20),labels=[1,1,0,0])
 m.drawmeridians(np.arange(-100,0,20),labels=[0,0,0,1])
-plt.title('YOLOSWAG')
+plt.title('Cascades')
 plt.show()
