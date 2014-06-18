c476f97f7a656fe8485aee98423fcec21adbf5b6
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-18 16:58:04 -0500

    getting windows correctly now

--- NetworkAutocorrelation/counting.py
+++ NetworkAutocorrelation/counting.py
@@ -2,6 +2,8 @@ import numpy as np
 import random as rand
 
 fakeSignal = rand.sample(range(-50,50), 20)
+print fakeSignal
+
 delta = 0.5
 
 window = 5
@@ -10,8 +12,16 @@ overlap = 1
 # transform fakeSignal to sliding windows
 
 totalDataPoints = len(fakeSignal)
-numPointsInWindow = window/delta
-numPointsInOverlap = overlap/delta
+numPointsInWindow = int(window/delta)
+numPointsInOverlap = int(overlap/delta)
 
 numWindows = int((totalDataPoints-numPointsInWindow)/numPointsInOverlap + 1)
-print numWindows
+
+splitToWindows = np.zeros(shape=(numWindows, numPointsInWindow))
+
+for i in xrange(numWindows):
+	start_index = i*numPointsInOverlap
+	for j in xrange(numPointsInWindow):
+		splitToWindows[i,j] = fakeSignal[start_index+j]
+
+print splitToWindows
