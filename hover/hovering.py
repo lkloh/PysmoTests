6a90a0a9de1f9e6e9548bb6fac91261ba6ba5018
Lay Kuan Loh <lkloh2410@gmail.com>
2014-06-20 19:11:10 -0500

    try to hover

---
+++ hover/hovering.py
@@ -0,0 +1,20 @@
+import matplotlib
+matplotlib.rcParams['backend'] = "TkAgg"
+import matplotlib.pyplot as py
+
+import numpy as npy
+from numpy.random import rand
+
+
+x, y, c, s = rand(4, 100)
+def onpick3(event):
+    ind = event.ind
+    print 'onpick3 scatter:', ind, npy.take(x, ind), npy.take(y, ind)
+
+fig = py.figure()
+ax1 = fig.add_subplot(111)
+col = ax1.scatter(x, y, 100*s, c, picker=True)
+#fig.savefig('pscoll.eps')
+fig.canvas.mpl_connect('pick_event', onpick3)
+
+py.show()
